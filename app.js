38d00966ed21ac8a2175903975378a2dad1b3224
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-07-26 22:28:36 +0530

    Save location to local storage

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+//Init storage
+const storage = new Storage();
+
+//Get stored location data
+const weatherLocation = storage.getLocationData();
+
 //Init Weather class
-const weather = new Weather('Pune', 'India');
+const weather = new Weather(weatherLocation.city, weatherLocation.country);
 
 //Init UI
 const ui = new UI();
@@ -7,11 +13,28 @@ const ui = new UI();
 //Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
 
+//Change location event
+document.getElementById('w-change-btn').addEventListener('click', e => {
+  const city = document.getElementById('city').value;
+  const country = document.getElementById('country').value;
+
+  //Change location
+  weather.changeLocation(city, country);
+
+  //Set location in localStorage
+  storage.setLocationData(city, country);
+
+  // Get and display weather
+  getWeather();
+
+  //Close Modal
+  $('#locModal').modal('hide');
+});
+
 function getWeather() {
   weather
     .getWeather()
     .then(results => {
-      console.log(results);
       ui.paint(results);
     })
     .catch(err => console.log(err));
